87cb42a2311f4ba677f66afb6b0b87d50edea2ef
Thiago Braga <thibraga06@gmail.com>
2023-11-27 14:11:18 -0300

    feat: New test.run script used by Github Actions and locally

--- test.run.sh
+++ test.run.sh
@@ -4,14 +4,32 @@ TAG="${1:-fpm-dev}"
 IMAGE_NAME=scaffold-php8
 DOCKERHUB_USERNAME=${DOCKERHUB_USERNAME:-thiagobraga}
 DOCKER_IMAGE="${DOCKERHUB_USERNAME}/${IMAGE_NAME}:${TAG}"
-COMMANDS=('whoami' 'php -v' 'php-fpm -v' 'composer -V')
-NGINX_COMMANDS=('nginx -v' 'nginx -t' 'supervisord version')
-QUALITY_COMMANDS=('phan -v' 'phpcpd -v' 'phpcs --version' 'phpcbf --version' 'php-cs-fixer -V' 'phpmd --version' 'phpstan -V' 'phpunit --version')
+PHP_COMMANDS=(
+  'whoami'
+  'php -v'
+  'php-fpm -v'
+  'composer -V'
+)
+NGINX_COMMANDS=(
+  'nginx -v'
+  'nginx -t'
+  'supervisord version'
+)
+QUALITY_COMMANDS=(
+  'phan -v'
+  'phpcpd -v'
+  'phpcs --version'
+  'phpcbf --version'
+  'php-cs-fixer -V'
+  'phpmd --version'
+  'phpstan -V'
+  'phpunit --version'
+)
 
 set -x
 
-for cmd in "${COMMANDS[@]}"; do docker run ${DOCKER_IMAGE} ${cmd}; done
-for cmd in "${COMMANDS[@]}"; do docker run -e ASUSER=0 ${DOCKER_IMAGE} ${cmd}; done
+for cmd in "${PHP_COMMANDS[@]}"; do docker run ${DOCKER_IMAGE} ${cmd}; done
+for cmd in "${PHP_COMMANDS[@]}"; do docker run -e ASUSER=0 ${DOCKER_IMAGE} ${cmd}; done
 
 if [ ${TAG} == 'fpm-dev' ]; then
   docker run ${DOCKER_IMAGE} php -m | grep xdebug
@@ -27,5 +45,4 @@ fi
 
 if [ ${TAG} == 'quality' ]; then
   for cmd in "${QUALITY_COMMANDS[@]}"; do docker run ${DOCKER_IMAGE} ${cmd}; done
-  for cmd in "${QUALITY_COMMANDS[@]}"; do docker run -e ASUSER=0 ${DOCKER_IMAGE} ${cmd}; done
 fi
