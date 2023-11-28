530fc674bd236300a6f46137f8bcf7c4428939dd
Thiago Braga <thibraga06@gmail.com>
2023-11-28 02:59:38 -0300

    feat: Make build and run scripts sequentially

--- test.build.sh
+++ test.build.sh
@@ -6,6 +6,6 @@ export BUILDKIT_PROGRESS=plain
 TAG="${1:-fpm-dev}"
 DOCKER_IMAGE=thiagobraga/scaffold-php8
 
-docker build -f ${TAG}/Dockerfile -t ${DOCKER_IMAGE}:${TAG} ${TAG}
-docker rmi -f $(docker images | grep '<none>' | awk '{ print $3 }') >/dev/null 2>&1
-docker images --format 'table {{.ID}}\t{{.Repository}}:{{.Tag}}\t{{.Size}}' | grep thiagobraga/${IMAGE_NAME}
+docker build -f ${TAG}/Dockerfile -t ${DOCKER_IMAGE}:${TAG} ${TAG} \
+  && docker rmi -f $(docker images | grep '<none>' | awk '{ print $3 }') >/dev/null 2>&1 || true \
+  && docker images --format 'table {{.Repository}}:{{.Tag}}\t{{.Size}}' | grep ${DOCKER_IMAGE}
--- test.run.sh
+++ test.run.sh
@@ -2,36 +2,55 @@
 
 TAG="${1:-fpm-dev}"
 DOCKER_IMAGE="thiagobraga/scaffold-php8:${TAG}"
-PHP_COMMANDS=('whoami' 'php -v' 'php-fpm -v' 'composer -V')
-NGINX_COMMANDS=('nginx -v' 'nginx -t' 'supervisord version')
-QUALITY_COMMANDS=(
-  'phan -v'
-  'phpcpd -v'
-  'phpcs --version'
-  'phpcbf --version'
-  'php-cs-fixer -V'
-  'phpmd --version'
-  'phpstan -V'
-  'phpunit --version'
-)
 
-for cmd in "${PHP_COMMANDS[@]}"; do docker run ${DOCKER_IMAGE} ${cmd}; done
-for cmd in "${PHP_COMMANDS[@]}"; do docker run -e ASUSER=0 ${DOCKER_IMAGE} ${cmd}; done
+docker run ${DOCKER_IMAGE} sh -c "whoami \
+  && php -v | head -n1 \
+  && php-fpm -v | head -n1 \
+  && composer -V"
 
-if [ ${TAG} == 'fpm-dev' ]; then
-  docker run ${DOCKER_IMAGE} php -m | grep xdebug
-  docker run -e ASUSER=0 ${DOCKER_IMAGE} php -m | grep xdebug
-  docker run -e ENABLE_XDEBUG=false ${DOCKER_IMAGE} php -m | grep xdebug || echo 'Xdebug not installed'
-  docker run -e ASUSER=0 -e ENABLE_XDEBUG=false ${DOCKER_IMAGE} php -m | grep xdebug || echo 'Xdebug not installed'
+docker run -e ASUSER=0 ${DOCKER_IMAGE} sh -c "whoami \
+  && php -v | head -n1 \
+  && php-fpm -v | head -n1 \
+  && composer -V"
+
+if [[ ${TAG} =~ 'fpm-dev' ]]; then
+  docker run ${DOCKER_IMAGE} php -m | grep xdebug \
+    && docker run -e ENABLE_XDEBUG=false ${DOCKER_IMAGE} php -m | grep xdebug || echo 'Xdebug not installed' \
+    && docker run -e ASUSER=0 ${DOCKER_IMAGE} php -m | grep xdebug \
+    && docker run -e ASUSER=0 -e ENABLE_XDEBUG=false ${DOCKER_IMAGE} php -m | grep xdebug || echo 'Xdebug not installed'
+fi
+
+if [[ ${TAG} =~ 'fpm-prod' ]]; then
+  docker run ${DOCKER_IMAGE} sh -c "whoami \
+    && php -m | grep -i opcache | cut -d' ' -f2" \
+  && docker run -e ASUSER=0 ${DOCKER_IMAGE} sh -c "whoami \
+    && php -m | grep -i opcache | cut -d' ' -f2"
 fi
 
 if [[ ${TAG} =~ 'nginx' ]]; then
-  for cmd in "${NGINX_COMMANDS[@]}"; do docker run ${DOCKER_IMAGE} sh -c "whoami && nginx -t && ps -aux | grep nginx"; done
-  for cmd in "${NGINX_COMMANDS[@]}"; do docker run ${DOCKER_IMAGE} ${cmd}; done
-  for cmd in "${NGINX_COMMANDS[@]}"; do docker run -e ASUSER=0 ${DOCKER_IMAGE} ${cmd}; done
+  docker run ${DOCKER_IMAGE} sh -c "nginx -v \
+    && supervisord version"
+  docker run -e ASUSER=0 ${DOCKER_IMAGE} sh -c "nginx -v \
+    && nginx -t \
+    && supervisord version"
 fi
 
-if [ ${TAG} == 'quality' ]; then
-  for cmd in "${QUALITY_COMMANDS[@]}"; do docker run ${DOCKER_IMAGE} ${cmd}; done
-  for cmd in "${QUALITY_COMMANDS[@]}"; do docker run -e ASUSER=0 ${DOCKER_IMAGE} ${cmd}; done
+if [[ ${TAG} == 'quality' ]]; then
+  docker run ${DOCKER_IMAGE} sh -c "phan -v \
+    && phpcpd -v \
+    && phpcs --version \
+    && phpcbf --version \
+    && php-cs-fixer -V \
+    && phpmd --version \
+    && phpstan -V \
+    && phpunit --version"
+
+  docker run -e ASUSER=0 ${DOCKER_IMAGE} sh -c "phan -v \
+    && phpcpd -v \
+    && phpcs --version \
+    && phpcbf --version \
+    && php-cs-fixer -V \
+    && phpmd --version \
+    && phpstan -V \
+    && phpunit --version"
 fi
